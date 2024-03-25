a5b9ba8fbddca5ecb2d4a756fde326c2c636f979
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-24 22:16:07 -0400

    feat: 03 - adding calculator logic

--- 03-calculator/src/App.jsx
+++ 03-calculator/src/App.jsx
@@ -1,34 +1,81 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { evaluate } from 'mathjs'
+
+const numbers = [
+  [1], [2], [3],
+  [4], [5], [6],
+  [7], [8], [9],
+  ['='], [0], ['.']
+]
+
+const operations = [
+  ['+'], ['-'],
+  ['*'], ['/']
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('')
+
+  const addInputNumber = (e) => {
+    const value = parseInt(e.target.getAttribute('data-value'))
+    if (!isNaN(value)) {
+      setInput(input + value)
+    }
+  }
+
+  const addOperator = (e) => {
+    const value = e.target.getAttribute('data-value')
+    setInput(input + value)
+  }
+
+  const handleResult = () => {
+    setInput(evaluate(input))
+  }
+
+  const handleDelete = () => {
+    setInput('')
+  }
 
   return (
-    <>
+    <div className="font-bold text-3xl">
+
+      <div className="mb-2 flex flex-row items-center justify-end rounded-md border-white border-2 w-[290px] h-[70px] px-2 overflow-x-auto">
+        {input}
+      </div>
+
+      <div className="grid grid-flow-col">
+        <div className="grid grid-cols-3 text-center">
+          {
+            numbers.map((number, index) => {
+              return (
+                <button onClick={(index === 9) ? handleResult : addInputNumber} data-value={number} className="p-5 m-1 rounded-md cursor-pointer border-white border-2" key={index}>{number}</button>
+              )
+            })
+          }
+        </div>
+        <div className="flex flex-col justify-around">
+          {
+            operations.map((op, index) => {
+              return (
+                <button
+                  onClick={addOperator}
+                  data-value={op}
+                  key={index}
+                  className="p-5 text-center w-[70px] rounded-md cursor-pointer border-white border-2 bg-gray-300 text-slate-900 hover:bg-gray-500 transition-colors"
+                >
+                  {op}
+                </button>
+              )
+            })
+          }
+        </div>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleDelete} className='bg-gray-200 w-full text-center p-2 rounded-md text-slate-900 text-sm hover:bg-gray-400 transition-colors'>
+          Clear
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
